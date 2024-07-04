01008abb88d7e5c0cbc963fd610fe1b6aaca8a56
navanee867 <navanee777@gmail.com>
2024-07-04 12:43:02 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,13 @@
+using System;
 
+public class StringCalculator
+{
+ public int Add(string input)
+ {
+     if (string.IsNullOrEmpty(input))
+     {
+         return 0;
+     }
+     return -1; 
+ }
+}
