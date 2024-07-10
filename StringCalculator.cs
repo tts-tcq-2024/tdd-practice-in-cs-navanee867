7391d3b4e2973fdf625f9e17ddf80cb689a1e4c9
navanee867 <navanee777@gmail.com>
2024-07-10 11:49:42 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -2,12 +2,33 @@ using System;
 
 public class StringCalculator
 {
- public int Add(string input)
- {
-     if (string.IsNullOrEmpty(input))
-     {
-         return 0;
-     }
-     return -1; 
- }
+ public static int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+            string[] delimiters = { ",", "\n", "\\", ";", "//" };
+            string[] numberArray = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = 0;
+
+            foreach (int x in from string number in numberArray
+                              let x = int.Parse(number)
+                              select x)
+            {
+                if (x < 0)
+                {
+                    throw new Exception("Negative numbers are not allowed: " + x);
+                }
+                if (x < 1000)
+                {
+                    sum += x;
+                }
+            }
+
+            return sum;
+        }
+    }
 }
